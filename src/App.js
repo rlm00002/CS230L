fcf1f7336bfc3064e83bf3ede2ec47df5ca9a51a
rlm00002 <rlm00002@mix.wvu.edu>
2024-02-25 21:33:03 -0500

    fixing code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h1>CS 230L</h1>
       <h2>Section - 001</h2>
-      <p>WVU ID: 800324926<p>
+      <p>WVU ID: 800324926</p>
       <p>Hi, I am Rae McDonald</p>
     </div>
   );
